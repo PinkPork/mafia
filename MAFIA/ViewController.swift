0e33d936a0cd8d5c56f70a1b3f7232d3274f7ccf
Santiago Carmona Gonzalez <santiago.carmona@l0t420r8w2fwc.na.rccl.com>
2017-12-18 15:24:12 -0500

    Initial Commit

---
+++ MAFIA/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MAFIA
+//
+//  Created by Santiago Carmona Gonzalez on 12/18/17.
+//  Copyright © 2017 Santiago Carmona Gonzalez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
