37adcc617a29ff512b40823d2dfb6010b1365f8c
Hugo Bernal <hugonobernal@gmail.com>
2018-01-05 16:55:15 -0500

    Se modificó la alerta para pasar la lista agregada al presenter y actualizarlo en la vista. Adicional, se implementó la eliminación de las listas en el presenter y en la vista. También, se adicionó el comportamiento de deshabilitar el boton de agregar, hasta que haya texto en el TextField.

--- MAFIA/Views/Controllers/ListPlayers/ListPlayersViewController.swift
+++ MAFIA/Views/Controllers/ListPlayers/ListPlayersViewController.swift
@@ -19,6 +19,7 @@ class ListPlayersViewController: UIViewController {
     private var listPlayers: [PlayersListMO] = [PlayersListMO]()
     private var presenter: ListPlayersPresenter!
     weak var gamePresenter: GamePresenter!
+    private var addListAction: UIAlertAction?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,17 +34,24 @@ class ListPlayersViewController: UIViewController {
     // MARK: - IBActions
     
     @IBAction func addLists(_ sender: Any) {
-        let alertController = UIAlertController(title: "Agregar Listas", message: "Ingrese las listas deseadas", preferredStyle: .alert)
-        alertController.addTextField(configurationHandler: nil)
-        let addButton = UIAlertAction(title: "Agregar", style: .default) { (action) in
+        let alertController = UIAlertController(title: "ADD_LIST_TITLE".localized(), message: "ADD_LIST_MESSAGE".localized(), preferredStyle: .alert)
+        alertController.addTextField { (textField) in
+            textField.delegate = self
+        }
+        addListAction = UIAlertAction(title: "ADD_LIST_ACTION".localized(), style: .default) { (action) in
             
             let textField = alertController.textFields?.first
-            textField?.text
+            guard let listName = textField?.text, !listName.isEmpty else {
+                print("No hay ningún texto")
+                return
+            }
+            self.presenter.createList(withName: listName)
+            
             
         }
-        let cancelButton = UIAlertAction(title: "Cancelar", style: .cancel)
+        let cancelButton = UIAlertAction(title: "CANCEL_ACTION".localized(), style: .cancel)
         
-        alertController.addAction(addButton)
+        alertController.addAction(addListAction!)
         alertController.addAction(cancelButton)
         
         self.present(alertController, animated: true, completion: nil)
@@ -61,6 +69,12 @@ class ListPlayersViewController: UIViewController {
         tableView.delegate = self
     }
     
+    private func updateAddButtonState(text: String?) {
+        // Disable the Add buttos if the text field is empty.
+        let emptyText = text ?? ""
+        addListAction?.isEnabled = !emptyText.isEmpty
+    }
+    
     /*
     // MARK: - Navigation
 
@@ -76,6 +90,11 @@ class ListPlayersViewController: UIViewController {
 // MARK: - ListPlayersView protocol conformance
 
 extension ListPlayersViewController: ListPlayersView {
+    func deleteList(listPlayer: PlayersListMO, indexPath: IndexPath) {
+        listPlayers.remove(at: indexPath.row)
+        tableView.reloadData()
+    }
+    
     func addNewList(listPlayer: PlayersListMO) {
         listPlayers.append(listPlayer)
         tableView.reloadData()
@@ -103,10 +122,43 @@ extension ListPlayersViewController: UITableViewDataSource {
     }
 }
 
+// MARK: - TableView Delegate
+
 extension ListPlayersViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let selectedList = listPlayers[indexPath.row]
         presenter.selectList(list: selectedList)
         gamePresenter.restartGame()
     }
+    
+    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+        return [UITableViewRowAction.init(style: .destructive, title: "DELETE_LIST_ACTION".localized(), handler: { [weak self] (_, indexPath) in
+            if let strongSelf = self {
+                strongSelf.presenter.deleteList(playersList: strongSelf.listPlayers[indexPath.row], indexPath: indexPath)
+                // strongSelf.presenter.deletePlayer(player: strongSelf.playersToDisplay[indexPath.row], indexPath: indexPath)
+            }
+        })]
+    }
 }
+
+// MARK: - TextField Delegate
+
+extension ListPlayersViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // Hide the keyboard.
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        updateAddButtonState(text: textField.text)
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        // Disable the Save button while editing.
+        addListAction?.isEnabled = false
+    }
+}
+
+
