af1e2d10e1711d79a22e45a6e003057666f43450
Hugo Bernal <hugonobernal@gmail.com>
2018-01-18 23:04:01 -0500

    #17

--- MAFIA/Views/Controllers/Game/GameViewController.swift
+++ MAFIA/Views/Controllers/Game/GameViewController.swift
@@ -72,7 +72,7 @@ class GameViewController: UIViewController {
     
     // MARK: - Navigation
     
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    // In a storyboard-based application, you will often want to do a little preparation before navigation.
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if let destinationViewController = segue.destination as? ListPlayersViewController {
             destinationViewController.gamePresenter = presenter
